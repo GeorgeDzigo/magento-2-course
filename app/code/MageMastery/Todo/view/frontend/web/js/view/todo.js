61894c4837712957d7baaf39d299c5c89cc79252
Giorgi Dzigoshvili <giorgi.dzigoshvili@scandiweb.com>
2022-02-12 22:39:23 +0200

    LES-23 - Initial Commit Of LES-23

---
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -0,0 +1,53 @@
+define([
+  'uiComponent',
+  'jquery'
+], function (Component, $) {
+  'use strict';
+
+  return Component.extend({
+    defaults: {
+      tasks: [
+        {id: 1, label: "Task 1", status: false},
+        {id: 2, label: "Task 2", status: false},
+        {id: 3, label: "Task 3", status: false},
+        {id: 4, label: "Task 4", status: true},
+      ],
+    },
+
+    initObservable: function () {
+      this._super().observe(['tasks']);
+
+      return this;
+    },
+
+    switchStatus: function (data, event) {
+      const taskId = $(event.target).data('id');
+
+      var items = this.tasks().map(function (task) {
+        if (task.id === taskId) {
+          task.status = !task.status;
+        }
+        return task
+      });
+
+      this.tasks(items);
+    },
+
+    deleteTask: function (taskId) {
+      var tasks = [];
+
+      if (this.tasks().length === 1) {
+        this.tasks(tasks);
+        return;
+      }
+
+      this.tasks().forEach(function (task) {
+        if (task.id !== taskId) {
+          tasks.push(task);
+        }
+      });
+
+      this.tasks(tasks);
+    },
+  });
+});
